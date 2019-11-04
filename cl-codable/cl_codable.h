044266471c0faa002f51327627c4ce0ffdd05b46
Konstantin Zabelin <konstantinz@mogo.ca>
2019-11-03 18:49:03 -0800

    Initial Commit

---
+++ cl-codable/cl_codable.h
@@ -0,0 +1,19 @@
+//
+//  cl_codable.h
+//  cl-codable
+//
+//  Created by Zapko on 2019-11-03.
+//  Copyright © 2019 Zababako. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for cl_codable.
+FOUNDATION_EXPORT double cl_codableVersionNumber;
+
+//! Project version string for cl_codable.
+FOUNDATION_EXPORT const unsigned char cl_codableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cl_codable/PublicHeader.h>
+
+
