560d7d6b0b88b4b0a01490a4d9bcaee250837579
Konstantin Zabelin <konstantinz@mogo.ca>
2019-11-03 19:03:39 -0800

    Preparing project

---
+++ cl-codable/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  cl-codable
+//
+//  Created by Zapko on 2019-11-03.
+//  Copyright © 2019 Zababako. All rights reserved.
+//
+
+import Foundation
---
+++ cl-codable/Parser_spec.swift
@@ -0,0 +1,9 @@
+//
+//  Parser_spec.swift
+//  cl-codableTests
+//
+//  Created by Zapko on 2019-11-03.
+//  Copyright © 2019 Zababako. All rights reserved.
+//
+
+import Foundation
