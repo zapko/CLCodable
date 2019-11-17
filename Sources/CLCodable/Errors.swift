4c6119f1eac65e09c3ced111b2adfddc8736639e
Konstantin Zabelin <konstantinz@mogo.ca>
2019-11-16 23:50:06 -0800

    Conforming to CustomNSError

--- Sources/CLCodable/Errors.swift
+++ Sources/CLCodable/Errors.swift
@@ -27,7 +27,7 @@ public enum CLReadError: Error {
     case literalConversionFailed(Context)
 }
 
-public enum CLPrintError: Error {
+public enum CLPrintError: Error, CustomNSError {
 
     public struct Context {
         let message: String
@@ -38,5 +38,25 @@ public enum CLPrintError: Error {
     }
 
     case literalConversionFailed(Context)
+
+
+    // MARK: - CustomNSError
+
+    public static let errorDomain = "com.zababako.cl-encodable.print"
+
+    /// The error code within the given domain.
+    public var errorCode: Int {
+        switch self {
+        case .literalConversionFailed: return 1
+        }
+    }
+
+    /// The user-info dictionary.
+    public var errorUserInfo: [String : Any] {
+        switch self {
+        case .literalConversionFailed(let context):
+            return [NSLocalizedDescriptionKey : "Encoding literal failed: '\(context.message)"]
+        }
+    }
 }
 
